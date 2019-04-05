32f5daaa316501fd9851a6d5d9be4add4a39fa85
Camila Rori <camila.a.rori@gmail.com>
2019-04-05 01:24:31 -0300

    TP1

--- TPs/TP1/main.c
+++ TPs/TP1/main.c
@@ -92,6 +92,14 @@ int main()
     return 0;
 }
 
+/**
+* @brief Imprime el menu de opciones
+* @param verificadorA Verifica que A se haya inicializado
+* @param verificadorB Verifica que B se haya inicializado
+* @param operandoA Entero ingresado por el usuario
+* @param operandoB Entero ingresado por el usuario
+* @return 0 sin errores
+*/
 int mensajeMenu(int verificadorA, int verificadorB, int operandoA, int operandoB)
 {
     if(verificadorA==0 && verificadorB==0)
--- TPs/TP1/utn.c
+++ TPs/TP1/utn.c
@@ -1,39 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+* @brief Solicita el ingreso de un entero
+* @param mensaje Mensaje a imprimir
+* @param valorIngresado Entero ingresado por el usuario
+* @return 0 sin errores, -1 parametro no valido
+*/
 int getInt(char *mensaje, int *valorIngresado)
 {
-    printf("%s",mensaje);
-    scanf("%d",valorIngresado);
-    return 0;
+    int retorno=-1;
+    if(mensaje!=NULL && valorIngresado!=NULL)
+    {
+        printf("%s",mensaje);
+        scanf("%d",valorIngresado);
+        retorno=0;
+    }
+    return retorno;
 }
 
+/**
+* @brief Realiza las operaciones de suma, resta, división, multiplicación y factorial de dos enteros
+* @param
+* @return 0 sin errores, -1 parametro no valido
+*/
 int calculoOperaciones(int operandoA, int operandoB, int *suma, int *resta, int *division, int *multiplicacion,int *factorialA,int *factorialB)
 {
-    *suma=operandoA+operandoB;
-    *resta=operandoA-operandoB;
-    if(operandoB!=0)
+    int retorno=-1;
+    if(suma!=NULL && resta!=NULL && division!=NULL && multiplicacion!=NULL && factorialA!=NULL && factorialB!=NULL)
     {
-        *division=operandoA/operandoB;
+        *suma=operandoA+operandoB;
+        *resta=operandoA-operandoB;
+        if(operandoB!=0)
+        {
+            *division=operandoA/operandoB;
+        }
+        *multiplicacion=operandoA*operandoB;
+        *factorialA=1;
+        *factorialB=1;
+        int i;
+        int j;
+        if(operandoA>=0)
+        {
+            for(i=1; i<=operandoA; i++)
+            {
+                *factorialA*=i;
+            }
+        }
+        if(operandoB>=0)
+        {
+            for(j=1; j<=operandoB; j++)
+            {
+                *factorialB*=j;
+            }
+        }
+        retorno=0;
     }
-    *multiplicacion=operandoA*operandoB;
-    *factorialA=1;
-    *factorialB=1;
-    int i;
-    int j;
 
-    for(i=1; i<=operandoA; i++)
-    {
-        *factorialA*=i;
-    }
-    for(j=1; j<=operandoB; j++)
-    {
-        *factorialB*=j;
-    }
-
-    return 0;
+    return retorno;
 }
 
+/**
+* @brief Imprime el resultado de las operaciones de suma, resta, división, multiplicación y factorial de dos enteros
+* @param
+* @return 0 sin errores
+*/
 int mostrarResultados(int operandoA, int operandoB, int suma, int resta, int division, int multiplicacion,int factorialA,int factorialB)
 {
     printf("\nEl resultado de %d+%d es: %d",operandoA,operandoB,suma);
@@ -46,8 +77,26 @@ int mostrarResultados(int operandoA, int operandoB, int suma, int resta, int div
     {
         printf("\nEl resultado de %d/%d es: %d",operandoA,operandoB,division);
     }
+
     printf("\nEl resultado de %d*%d es: %d",operandoA,operandoB,multiplicacion);
-    printf("\nEl factorial de %d es: %d y El factorial de %d es: %d\n",operandoA,factorialA,operandoB,factorialB);
+
+    if(operandoA<0)
+    {
+        printf("\nNo es posible calcular el factorial de %d ya que es un numero negativo",operandoA);
+    }
+    else
+    {
+        printf("\nEl factorial de %d es: %d",operandoA,factorialA);
+    }
+
+    if(operandoB<0)
+    {
+        printf("\nNo es posible calcular el factorial de %d ya que es un numero negativo",operandoB);
+    }
+    else
+    {
+        printf("\nEl factorial de %d es: %d\n",operandoB,factorialB);
+    }
 
     return 0;
 }
